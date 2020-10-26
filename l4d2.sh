5fc4a164eac2490d1e690052adc1ebb6f0dbcebd
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2020-10-26 11:40:16 -0400

    Double quote $HOME expansion
    https://github.com/koalaman/shellcheck/wiki/SC2086

--- l4d2.sh
+++ l4d2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd $HOME/hlserver || exit
+cd "$HOME/hlserver" || exit
 l4d2/srcds_run -game left4dead2 -tickrate 100 -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/l4d2_ds.txt "$@"
--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd $HOME/hlserver || exit
+cd "$HOME/hlserver" || exit
 ./steamcmd.sh +runscript l4d2_ds.txt
